5dcf11930ad157732fafe7496ece02be200d5626
ren1244 <n1244506804@hotmail.com>
2022-12-10 17:04:08 +0800

    fix autoload

--- scripts/addFont.php
+++ scripts/addFont.php
@@ -1,23 +1,33 @@
 <?php
-require(__dir__ .'/../vendor/autoload.php');
+spl_autoload_register(function ($class_name) {
+    $arr = explode('\\', $class_name);
+    if(count($arr)>2 && $arr[0] === 'ren1244' && $arr[1] === 'PDFWriter') {
+        $arr[0] = __DIR__;
+        $arr[1] = '../src';
+        require(implode('/',$arr).'.php');
+    } else {
+        echo 'cannot autoload class: '.$class_name;
+        die();
+    }
+});
 
 use ren1244\PDFWriter\FontLib\FontLoader;
 
-$count=count($argv);
-if($count===2) {
-    $fname=getcwd().'/'.parse_url($argv[1])['path'];
-    if(!file_exists($fname) || is_dir($fname)) {
+$count = count($argv);
+if ($count === 2) {
+    $fname = getcwd() . '/' . parse_url($argv[1])['path'];
+    if (!file_exists($fname) || is_dir($fname)) {
         exit("file $fnamenot not exists");
     }
 
-    $ttf=new FontLoader;
+    $ttf = new FontLoader;
     $ttf->loadFile($fname);
-} elseif($count===3) {
-    $fname=getcwd().'/'.parse_url($argv[1])['path'];
-    $outname=$argv[2];
-    if(!file_exists($fname) || is_dir($fname)) {
+} elseif ($count === 3) {
+    $fname = getcwd() . '/' . parse_url($argv[1])['path'];
+    $outname = $argv[2];
+    if (!file_exists($fname) || is_dir($fname)) {
         exit("file $fnamenot not exists");
     }
-    $ttf=new FontLoader;
+    $ttf = new FontLoader;
     $ttf->loadFile($fname, $outname);
 }
